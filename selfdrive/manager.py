5087f4cf44052ef921ab27e109cc317fa6cdfaef
Willem Melching <willem.melching@gmail.com>
2020-09-10 15:12:53 +0200

    use light sensor from sensord

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -223,6 +223,7 @@ if not PC:
   persistent_processes += [
     'logcatd',
     'tombstoned',
+    'sensord',
   ]
 
 if ANDROID:
@@ -259,7 +260,6 @@ if WEBCAM:
 if not PC:
   car_started_processes += [
     'ubloxd',
-    'sensord',
     'dmonitoringd',
     'dmonitoringmodeld',
   ]
